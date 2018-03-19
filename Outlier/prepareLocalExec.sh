c6edd26a58ba396ca4b3d066360942f13cd4165c
THINH DOAN VU <thinhdv@ntu.edu.vn>
2018-03-19 13:00:25 +0700

    Delete prepareLocalExec.sh

--- Outlier/prepareLocalExec.sh
+++
@@ -1,7 +0,0 @@
-mkdir scratch 2>/dev/null
-cp support/*example* scratch
-cd scratch
-unzip ../support/allfdist >/dev/null
-cd ../src/java
-javac -cp . -source 1.6 -target 1.6 temporal/Simulator.java
-javac -cp . -source 1.6 -target 1.6 temporal/Datacal.java
